c4ec66df43176de591bba39531fc2470cf65be31
Natasha Musatova <natali.schimpl@gmail.com>
2025-01-20 22:21:50 +0300

    Run on GPU

--- src/model.py
+++ src/model.py
@@ -2,6 +2,7 @@ import pandas as pd
 import tensorflow as tf
 from tensorflow.keras import optimizers, Sequential
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dropout, Flatten, Dense
+from loguru import logger
 
 from settings import MODEL_DIR, EPOCHS, BREEDS_LIST
 
@@ -16,8 +17,22 @@ class ModelBuilder:
     def train(self) -> tf.keras.Model:
         """Train and save model"""
         model = self.model_architecture()
-        model.fit(self.train_ds, epochs=EPOCHS)
+
+        # Проверим доступность GPU
+        physical_devices = tf.config.list_physical_devices("GPU")
+        if physical_devices:
+            print("GPU Available: ", physical_devices)
+        else:
+            print("GPU not available, using CPU instead.")
+
+        # Установим устройство для вычислений
+        device_cpu = "/CPU:0"
+        device_gpu = "/GPU:0" if physical_devices else "/CPU:0"
+
+        with tf.device(device_gpu):
+            model.fit(self.train_ds, epochs=EPOCHS, validation_split=0.2)
         self.save_model(model)
+        y_pred_prob = model.predict(self.train_ds)
         return model
 
     def model_architecture(self) -> tf.keras.Model:
@@ -43,8 +58,9 @@ class ModelBuilder:
         model.compile(
             loss=tf.keras.losses.CategoricalCrossentropy(),
             optimizer=optimizers.Adam(learning_rate=0.0001),
-            metrics=["accuracy"],
+            metrics=["Accuracy", "Precision", "Recall"],
         )
+        logger.info(f"model:{model.summary()}")
         return model
 
     def save_model(self, model: tf.keras.Model):
