4ded6adcce78a74aae746903a8bdb05a422f5d00
Natasha Musatova <natali.schimpl@gmail.com>
2025-01-20 22:25:09 +0300

    Add mlflow tracking_uri

--- src/settings.py
+++ src/settings.py
@@ -1,14 +1,16 @@
 import os.path
 from pathlib import Path
 
+# MLflow
+TRACKING_URI = "http://13.60.52.168:5000"
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-DATA_DIR = BASE_DIR / "data2"
-# BREEDS_LIST = os.listdir(DATA_DIR)
-BREEDS_LIST = ["Abyssinian", "Bengal", "Bombay"]
+DATA_DIR = BASE_DIR / "data"
+BREEDS_LIST = os.listdir(DATA_DIR)
+# BREEDS_LIST = ["Abyssinian", "Bengal", "Bombay"]
 MODEL_DIR = BASE_DIR / "model"
 PREDICT_DIR = BASE_DIR / "predict"
 if not os.path.isdir(MODEL_DIR):
     os.mkdir(MODEL_DIR)
 
-EPOCHS = 2
+EPOCHS = 20
